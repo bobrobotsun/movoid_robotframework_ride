d3d79afb165d5849abfb863affc223d4e74c8040
movoid <bobrobotsun@163.com>
2024-06-05 00:48:30 +0800

    2024.06.05.01-movoid 更新1.0.2 1、新增了通过创建bat的方法启动ride的方法

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_robotframework_ride',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='',
     license='',
